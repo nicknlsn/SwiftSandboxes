30ade8c02cd7a8d74dde615743a97c4a79567b79
Nicholas Nelson <nick@pro.local>
2016-02-03 11:06:04 -0700

    extensions

---
+++ SBExtensionsApp/SBExtensionsApp/ViewController.swift
@@ -0,0 +1,156 @@
+//
+//  ViewController.swift
+//  SBExtensionsApp
+//
+//  Created by Nicholas Nelson on 2/2/16.
+//  Copyright © 2016 Nicholas Nelson. All rights reserved.
+//
+
+import UIKit
+
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m:  Double { return self           }
+    var cm: Double { return self / 100.0   }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+extension Int {
+    // a method
+    func repetitions(task: () -> Void) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+    
+    // a mutating instance method
+    mutating func square() {
+        self = self * self
+    }
+    
+    // subscripts
+    subscript(var digitIndex: Int) -> Int {
+        var decimalBase = 1
+        while digitIndex > 0 {
+            decimalBase *= 10
+            --digitIndex
+        }
+        return (self / decimalBase) % 10
+    }
+    
+    // nested type
+    enum Kind {
+        case Negative, Zero, Positive
+    }
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .Zero
+        case let x where x > 0:
+            return .Positive
+        default:
+            return .Negative
+        }
+    }
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        var str = "Hello, playground"
+        
+        class SomeType {
+            var attribute = "some text"
+            
+            func behavior(aString: String) -> Void {}
+        }
+
+        let oneInch = 25.4.mm
+        print("One inch is \(oneInch) meters")
+        let threeFeet = 3.ft
+        print("Three feet is \(threeFeet) meters")
+        
+        let aMarathon = 42.km + 195.m
+        print("A marathod is \(aMarathon) meters long")
+        
+        /*******************************************************************/
+        
+        let defaultRext = Rect()
+        let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+        
+        let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+        
+        /*******************************************************************/
+        
+        3.repetitions({
+        print("Hello!")
+        })
+        
+        // method
+        3.repetitions {
+            print("Goodbye")
+        }
+        
+        // mutating instance method
+        var someInt = 3
+        someInt.square()
+        
+        // subscripts
+        746381295[0]
+        746381295[1]
+        746381295[2]
+        746381295[8]
+        746381295[9]
+        
+        // nested type
+        func printIntegerKinds(numbers: [Int]) {
+            for number in numbers {
+                switch number.kind {
+                case .Negative:
+                    print("- ", terminator: "")
+                case .Zero:
+                    print("0 ", terminator: "")
+                case .Positive:
+                    print("+ ", terminator: "")
+                }
+            }
+            print("")
+        }
+        
+        printIntegerKinds([3, 19, -27, 0, -6, 0, 7])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
